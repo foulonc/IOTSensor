d9230037dee052b837649911376b20e90d7ab6c1
foulonc <46761934+foulonc@users.noreply.github.com>
2020-07-26 11:57:40 +0200

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,4 @@
+char ssid[] = "SSID";                          	// your network SSID (name)
+char pass[] = "WIFI_PASSWORD";               		// your network password (use for WPA, or use as key for WEP)
+const char key[] = "MQTT_Broker_Key";           // broker key  
+const char secret[] = "MQTT_Broker_Secret";     // broker secret  
